4d10c1f4b8f0010a9d0c7a59ce63fe42536a8d15
patt26 <kharchepratik123@gmail.com>
2021-05-19 22:34:41 +0530

    Added Multiple address books

--- AddressBook/AddressBook/MainBook.cs
+++ AddressBook/AddressBook/MainBook.cs
@@ -8,25 +8,36 @@ namespace AddressBook
 {
     class MainBook
     {
-        private List<Entry> list { get; set; } = new List<Entry>();
-        public void AddContact(Entry contact)
+        public static List<Entry> list = new List<Entry>();
+        public static Dictionary<string, Entry> dictionary = new Dictionary<string, Entry>();
+        public void Add(Entry contact)
         {
             list.Add(contact);
+           
         }
 
         public void DisplayList()
         {
-            foreach (var Entry in list)
+            foreach (var element in dictionary)
             {
-                Console.WriteLine(Entry.FirstName+" "+Entry.LastName+" "+Entry.PhoneNum+" "+Entry.Address+" "+Entry.City+" "+Entry.PinCode+" "+Entry.State+" " +Entry.EmailId);
+                Console.WriteLine($" Address Book: {element.Key}\n First Name: {element.Value.FirstName}\n Last Name: {element.Value.LastName}\n Phone Number: {element.Value.PhoneNum}\n Address: {element.Value.Address}\n City: {element.Value.City}\n Pin Code: {element.Value.PinCode}\n State: {element.Value.State}\n Email ID: {element.Value.EmailId}\n");
             }
         }
+        
 
-        public void EditName(string Name)
+        public static void EditName()
         {
-            foreach (var Entry in list)
+            Console.WriteLine("Available Address Books: ");
+            foreach(var key in dictionary)
             {
-                if (Entry.FirstName == Name)
+                Console.WriteLine(key.Key);
+            }
+            Console.WriteLine("Name of Book You Want to Edit: ");
+            string book = Console.ReadLine();
+            
+            foreach (var element in dictionary)
+            {
+                if (element.Key == book)
                 {
                     Console.WriteLine("Enter Updated First Name: ");
                     string nfirstName = Console.ReadLine();
@@ -48,13 +59,15 @@ namespace AddressBook
                 }
                 else
                 {
-                    Console.WriteLine("Entry is Missing!!!");
+                    Console.WriteLine("Address Book is Missing!!!");
                 }
                 
             }
         }
-        public void DeleteEntries(string Name2)
+        public static void DeleteEntries()
         {
+            Console.WriteLine("Enter Name You Want To Delete:- ");
+            string Name2 = Console.ReadLine();
             foreach (var Entry in list)
             {
                 if (Entry.FirstName == Name2)
@@ -64,6 +77,31 @@ namespace AddressBook
                     break;
                 }
             }
+        }
+        public static void Save()
+        {
+            Console.WriteLine("Enter Name for Address Book: ");
+            string BookName = Console.ReadLine();
+
+            Console.Write("Enter First Name- ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter Last Name- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Phone Number- ");
+            string phoneNum = Console.ReadLine();
+            Console.Write("Enter Address- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City Name- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter Your Pincode- ");
+            string pinCode = Console.ReadLine();
+            Console.Write("Enter State- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Email ID- ");
+            string emailId = Console.ReadLine();
+
+            var newEntry = new Entry(firstName, lastName, phoneNum, address, city, pinCode, state, emailId);
+            dictionary.Add(BookName, newEntry);
         }
        
     } 
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -17,39 +17,17 @@ namespace AddressBook
             while (true)
             {
 
-
                 switch (choice)
                 {
                     case "1":
-                        Console.Write("Enter First Name- ");
-                        string firstName = Console.ReadLine();
-                        Console.Write("Enter Last Name- ");
-                        string lastName = Console.ReadLine();
-                        Console.Write("Enter Phone Number- ");
-                        string phoneNum = Console.ReadLine();
-                        Console.Write("Enter Address- ");
-                        string address = Console.ReadLine();
-                        Console.Write("Enter City Name- ");
-                        string city = Console.ReadLine();
-                        Console.Write("Enter Your Pincode- ");
-                        string pinCode = Console.ReadLine();
-                        Console.Write("Enter State- ");
-                        string state = Console.ReadLine();
-                        Console.Write("Enter Email ID- ");
-                        string emailId = Console.ReadLine();
-
-                        var newEntry = new Entry(firstName, lastName, phoneNum, address, city, pinCode, state, emailId);
-                        person.AddContact(newEntry);
+                        MainBook.Save();
                         break;
                     case "2":
-                        Console.WriteLine("Enter Name You Want to edit: ");
-                        string Name = Console.ReadLine();
-                        person.EditName(Name);
+                        MainBook.EditName();
                         break;
                     case "3":
-                        Console.WriteLine("Enter Name You Want To Delete:- ");
-                        string Name2 = Console.ReadLine();
-                        person.DeleteEntries(Name2);
+                       
+                        MainBook.DeleteEntries();
                         break;
                     case "4":
                         person.DisplayList();
@@ -60,7 +38,7 @@ namespace AddressBook
 
                 }
 
-                Console.WriteLine("Select Options\n 1:Add new Entry\n 2:Edit Entry\n 3:Delete Entry\n 4:Display Entry");
+                Console.WriteLine("Select Options\n 1:Add new Entry\n 2:Edit Entry\n 3:Delete Entry\n 4:Display Entry\n 5:Exit");
                 choice = Console.ReadLine();
             
                 
