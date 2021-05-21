00ed279ea92f002fa7132d2fb2d404f08f5e3dfe
patt26 <kharchepratik123@gmail.com>
2021-05-21 19:48:00 +0530

    Added CSV file

--- AddressBook/AddressBook/FileIo.cs
+++ AddressBook/AddressBook/FileIo.cs
@@ -9,23 +9,38 @@ namespace AddressBook
 {
     class FileIo // TODO
     {
+        const string TEXT = @"C:\Users\prat\source\Assignment\AddressBook\AddressBook\AddressBook\StoreText.txt";
+        const string CSV = @"C:\Users\prat\source\Assignment\AddressBook\AddressBook\AddressBook\StoreCSV.csv";
         public static void WriteToText(Dictionary<string, Entry> dictionary)
         {
-            const string TEXT = @"C:\Users\prat\source\Assignment\AddressBook\AddressBook\AddressBook\StoreText.txt";
-                        
-                if (File.Exists(TEXT))
+            if (File.Exists(TEXT))
+            {
+                using (TextWriter tw = File.AppendText(TEXT))
                 {
-                    using (TextWriter tw = File.AppendText(TEXT))
+                    tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,PinCode,PhoneNum,Email"));
+                    foreach (var element in dictionary)
                     {
-                        tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,PinCode,PhoneNum,Email"));
-                        foreach (var element in dictionary)
-                        {
-                            tw.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", element.Value.FirstName, element.Value.LastName, element.Value.Address, element.Value.City, element.Value.State, element.Value.PinCode, element.Value.PhoneNum, element.Value.EmailId));
-                        }
-                       
+                        tw.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", element.Value.FirstName, element.Value.LastName, element.Value.Address, element.Value.City, element.Value.State, element.Value.PinCode, element.Value.PhoneNum, element.Value.EmailId));
                     }
+                       
                 }
-            
+            }
+        }
+
+        public static void WriteToCSV(Dictionary<string, Entry> dictionary)
+        {
+            if (File.Exists(TEXT))
+            {
+                using (TextWriter tw = File.AppendText(CSV))
+                {
+                    tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,PinCode,PhoneNum,Email"));
+                    foreach (var element in dictionary)
+                    {
+                        tw.WriteLine(string.Format("{0},{1},{2},{3},{4},{5},{6},{7}", element.Value.FirstName, element.Value.LastName, element.Value.Address, element.Value.City, element.Value.State, element.Value.PinCode, element.Value.PhoneNum, element.Value.EmailId));
+                    }
+
+                }
+            }
         }
     }
 }
